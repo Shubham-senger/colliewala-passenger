86d3a02867a0dd560e3993d32e412ed652e6903f
kooliewale <cooliewale+github@gmail.com>
2024-06-22 16:55:36 +0530

    Update cards.jsx

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import coolie_img from "../assets/porter_circle.png";
+import order_info from "../assets/order_info.png";
 function Card() {
   return (
     <div className="flex justify-center items-center h-full p-4">
@@ -8,7 +9,7 @@ function Card() {
         <div className="card bg-gradient-to-r from-blue-400 to-blue-600 backdrop-blur-lg rounded-2xl shadow-xl p-8 text-center text-white transform transition-transform hover:scale-105 hover:shadow-2xl">
           <h2 className="text-3xl font-bold mb-4">Book Your Collie</h2>
           <img
-            src="https://via.placeholder.com/150"
+            src={coolie_img}
             alt="Collie"
             className="rounded-2xl mb-4 mx-auto"
           />
@@ -22,7 +23,7 @@ function Card() {
         <div className="card bg-gradient-to-r from-blue-400 to-blue-600 backdrop-blur-lg rounded-2xl shadow-xl p-8 text-center text-white transform transition-transform hover:scale-105 hover:shadow-2xl">
           <h2 className="text-3xl font-bold mb-4">Order Status</h2>
           <img
-            src="https://via.placeholder.com/150"
+            src={order_info}
             alt="Order Status"
             className="rounded-2xl mb-4 mx-auto"
           />
